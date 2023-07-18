a6611f0fd533edae025557b7c1a98b6d5b570e12
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2023-07-19 01:15:25 +0200

    useContext - react-router-dom setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import { HooksApp } from './HooksApp'
 import { CounterApp } from './01-useState/CounterApp'
@@ -13,12 +14,15 @@ import { Memorize } from './06-memos/Memorize'
 import { MemorizeHook } from './06-memos/MemorizeHook'
 import { CallbackHook } from './06-memos/CallbackHook'
 import { Super } from './07-memo-task/Super'
-import { TodoApp } from './07-useReducer/TodoApp'
-
 // import './07-useReducer/intro-reducer';
+import { TodoApp } from './07-useReducer/TodoApp'
+import { MainApp } from './08-useContext/MainApp'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TodoApp />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <BrowserRouter>
+    <MainApp />
+  </BrowserRouter>
+  // </React.StrictMode>,
 )
